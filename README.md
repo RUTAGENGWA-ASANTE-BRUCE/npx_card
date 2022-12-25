9fb52ed70498eb05ca3901316659e636dcacb7ec
RUTAGENGWA-ASANTE-BRUCE <rutagengwabruce@gmail.com>
2022-12-26 01:28:34 +0200

    [feat] npx card

---
+++ README.md
@@ -0,0 +1,23 @@
+# NPX CARD
+This my NPX card unique style to connect with me directly via console or terminal
+
+👇 just hit 
+```bash
+npx rutagengwabruce
+```
+And get to know me in unique style.
+
+### I spent a non-trivial amount of effort building and designing this iteration of npx card, and I am proud of it! All I ask of you all is to put a **star** ⭐ on this project and not claim this effort as your own ♥.
+
+### SCREENSHOT
+
+The final output might look something like this:
+
+![image](https://github.com/RUTAGENGWA-ASANTE-BRUCE/npx_card/blob/master/demo.gif)
+
+
+<hr/>
+
+##### STEPS TO CREATE YOUR OWN
+The article written by our friend @jackboberg. I used the same for the reference to deploy the package. 
+[Write a Simple npx Business Card](https://studioelsa.se/blog/open-source-oss-npx-business-card). 
---
+++ card.js
@@ -0,0 +1,128 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:hi@rutagengwa-asante-bruce.vercel.app");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+                value: () => {
+                    // cliSpinners.dots;
+                    const loader = ora({
+                        text: ' Downloading Resume',
+                        spinner: cliSpinners.material,
+                    }).start();
+                    let pipe = request('https://drive.google.com/file/d/1wFEAabbCbo4oE81VO0fCDfWAHxLACQTT/view?usp=sharing').pipe(fs.createWriteStream('./rutagengwabruce-resume.html'));
+                    pipe.on("finish", function () {
+                        let downloadPath = path.join(process.cwd(), 'rutagengwabruce-resume.html')
+                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
+                        open(downloadPath)
+                        loader.stop();
+                    });
+                }
+            },
+            {
+                name: `Schedule a ${chalk.redBright.bold("Meeting")}?`,
+                value: () => {
+                    open('https://calendly.com/rutagengwabruce/30min');
+                    console.log("\n See you at the meeting \n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("             RUTAGENGWA asante Bruce"),
+    handle: chalk.white("@RUTAGENGWA-ASANTE-BRUCE"),
+    work: `${chalk.white("Full stack")} ${chalk
+        .hex("#2b82b2")
+        .bold("Software Enginner")}`,
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("rutagengwabruce"),
+    github: chalk.gray("https://github.com/") + chalk.green("RUTAGENGWA-ASANTE-BRUCE"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("bruce-asante-2b072822b"),
+    web: chalk.cyan("https://rutagengwa-asante-bruce.vercel.app/"),
+    npx: chalk.red("npx") + " " + chalk.white("rutagengwabruce"),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I am currently looking for new opportunities,"
+        )}`,
+        `${chalk.italic("my inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
